9dab08f0711e8e6a7f3c39321f8b35bcab305e5c
sebasferruiz <36142019+sebasferruiz@users.noreply.github.com>
2018-07-20 12:45:08 -0500

    Create stop.py

---
+++ Documents/stop.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import LaserScan
+from ackermann_msgs.msg import AckermannDriveStamped
+
+class stop():
+	def __init__(self):
+		rospy.Subscriber('/scan', LaserScan, callback)
+		rospy.Publisher('/vesc/ackermann_cmd_mux/input/navigation', AckermannDriveStamped, queue_size=10)		
+
+	def callback(msg):
+		print "STOP"
+		msg_a = AckermannDriveStamped()
+		msg_a.drive.speed = 0.0
+		msg_a.drive.steering_angle = 0.0
+		msg_a.header.stamp = rospy.Time.now()
+		pub.publish(msg_a)
+
+
+if __name__ == "__main__":
+    rospy.init_node("stop", anonymous = True)
+    node =stop()
+    rospy.spin()
