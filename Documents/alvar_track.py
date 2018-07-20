7550edd18ce8b43913d6d0dce9bbcced067ab180
sebasferruiz <36142019+sebasferruiz@users.noreply.github.com>
2018-07-20 12:38:57 -0500

    Update alvar_track.py

--- Documents/alvar_track.py
+++ Documents/alvar_track.py
@@ -3,15 +3,23 @@
 import rospy
 #from sensor_msgs.msg import Image
 from ar_track_alvar_msgs.msg import AlvarMarkers
+import os
+import time
 
 class ar_switch():
     def __init__(self):
         rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.callback, queue_size = 1)
         #rospy.Subscriber("/camera/rgb/image_rect_color", Image, self.callback,  queue_size = 1)
 
-    def callback(self,msg):
-        if msg.header.frame_id != 1:
-            print ("AR tag" + msg.header.frame_id)   
+    def callback(self,marker):
+        if len(marker.markers) > 0:
+            if marker.markers[0].id == 3:
+                time.sleep(3)
+                os.system('go.py')
+            if mafker.markers[0].id == 4:
+                time.sleep(3)
+                os.system('stop.py')
+                   
 
 if __name__ == "__main__":
     rospy.init_node("ar_switch", anonymous = True)
